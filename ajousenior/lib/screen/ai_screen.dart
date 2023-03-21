233780835bdd11442c55f3e8f793ba035ec1480f
KoKimss <seungsu8848@ajou.ac.kr>
2023-03-22 00:59:09 +0900

    Feat: AiScreen, Add Speechtotext(test)

--- ajousenior/lib/screen/ai_screen.dart
+++ ajousenior/lib/screen/ai_screen.dart
@@ -1,10 +1,101 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
-class AiScreen extends StatelessWidget {
+class AiScreen extends StatefulWidget {
   const AiScreen({super.key});
 
   @override
+  State<AiScreen> createState() => _AiScreenState();
+}
+
+class _AiScreenState extends State<AiScreen> {
+  SpeechToText speechToText = SpeechToText();
+  var text = "버튼을 누르면서 말해주세요";
+  var isListening = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        endRadius: 75.0,
+        animate: isListening,
+        duration: const Duration(milliseconds: 2000),
+        glowColor: Theme.of(context).colorScheme.secondaryContainer,
+        repeat: true,
+        repeatPauseDuration: const Duration(milliseconds: 100),
+        showTwoGlows: true,
+        child: GestureDetector(
+          onTapDown: (details) async {
+            if (!isListening) {
+              var available = await speechToText.initialize();
+              if (available) {
+                setState(() {
+                  isListening = true;
+                  speechToText.listen(
+                    onResult: (result) {
+                      setState(() {
+                        text = result.recognizedWords;
+                      });
+                    },
+                  );
+                });
+              }
+            }
+          },
+          onTapUp: (details) {
+            setState(() {
+              isListening = false;
+            });
+            speechToText.stop();
+          },
+          child: CircleAvatar(
+            backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+            radius: 35,
+            child: Icon(
+              isListening ? Icons.mic : Icons.mic_none,
+              color: Colors.grey,
+            ),
+          ),
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+        leading: const Icon(
+          Icons.sort_rounded,
+          color: Colors.white,
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+        title: Text(
+          'AI',
+          style: GoogleFonts.notoSansKannada(
+            fontSize: 40,
+            color: Theme.of(context).colorScheme.outline,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        reverse: true,
+        physics: const BouncingScrollPhysics(),
+        child: Container(
+          color: Theme.of(context).colorScheme.secondaryContainer,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height * 0.7,
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          margin: const EdgeInsets.only(bottom: 150),
+          child: Text(
+            text,
+            style: GoogleFonts.notoSansKannada(
+              fontSize: 30,
+              color: Theme.of(context).colorScheme.outline,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- ajousenior/lib/screen/volunteer_screen.dart
+++ ajousenior/lib/screen/volunteer_screen.dart
@@ -5,6 +5,18 @@ class VolunteerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 2,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: const Center(
+          child: Text(
+            "Senior App",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
+          ),
+        ),
+      ),
+    );
   }
 }
