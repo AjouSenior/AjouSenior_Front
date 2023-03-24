02f8aab9ac8bec8125e93c5eeaecd620fc1e97c8
KoKimss <seungsu8848@ajou.ac.kr>
2023-03-24 21:40:43 +0900

    Feat : add GPT service

--- ajousenior/lib/screen/ai_screen.dart
+++ ajousenior/lib/screen/ai_screen.dart
@@ -1,3 +1,5 @@
+import 'package:ajousenior/models/chat_model.dart';
+import 'package:ajousenior/services/gpt_service.dart';
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -14,6 +16,15 @@ class _AiScreenState extends State<AiScreen> {
   SpeechToText speechToText = SpeechToText();
   var text = "버튼을 누르면서 말해주세요";
   var isListening = false;
+
+  final List<ChatMessage> messages = [];
+
+  var scrollController = ScrollController();
+
+  scrollMethod() {
+    scrollController.animateTo(scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300), curve: Curves.easeOut);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,11 +56,18 @@ class _AiScreenState extends State<AiScreen> {
               }
             }
           },
-          onTapUp: (details) {
+          onTapUp: (details) async {
             setState(() {
               isListening = false;
             });
             speechToText.stop();
+
+            messages.add(ChatMessage(text: text, type: ChatMessageType.user));
+            var msg = await GptService.sendMessage(text);
+
+            setState(() {
+              messages.add(ChatMessage(text: msg, type: ChatMessageType.bot));
+            });
           },
           child: CircleAvatar(
             backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
@@ -70,32 +88,108 @@ class _AiScreenState extends State<AiScreen> {
         centerTitle: true,
         elevation: 0.0,
         title: Text(
-          'AI',
+          'Voice Assistant',
           style: GoogleFonts.notoSansKannada(
             fontSize: 40,
             color: Theme.of(context).colorScheme.outline,
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        reverse: true,
-        physics: const BouncingScrollPhysics(),
-        child: Container(
-          color: Theme.of(context).colorScheme.secondaryContainer,
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * 0.7,
-          alignment: Alignment.center,
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-          margin: const EdgeInsets.only(bottom: 150),
-          child: Text(
-            text,
-            style: GoogleFonts.notoSansKannada(
-              fontSize: 30,
-              color: Theme.of(context).colorScheme.outline,
+      body: Container(
+        color: Theme.of(context).colorScheme.secondaryContainer,
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+        child: Column(
+          children: [
+            Text(
+              text,
+              style: GoogleFonts.notoSansKannada(
+                fontSize: 30,
+                color: isListening ? Colors.black : Colors.red,
+                fontWeight: FontWeight.w600,
+              ),
             ),
-          ),
+            const SizedBox(
+              height: 12,
+            ),
+            Expanded(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.white,
+                ),
+                child: ListView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    controller: scrollController,
+                    shrinkWrap: true,
+                    itemCount: messages.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      var chat = messages[index];
+
+                      return chatBubble(chattext: chat.text, type: chat.type);
+                    }),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Text(
+              "Developed by babadevs",
+              style: GoogleFonts.notoSansKannada(
+                fontSize: 30,
+                color: Theme.of(context).colorScheme.outline,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                  decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Colors.white,
+              )),
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+Widget chatBubble({required chattext, required ChatMessageType? type}) {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      CircleAvatar(
+        backgroundColor: Colors.amber.shade50,
+        child: const Icon(
+          Icons.person,
+          color: Color.fromARGB(255, 145, 91, 10),
+        ),
+      ),
+      const SizedBox(
+        width: 12,
+      ),
+      Expanded(
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.only(bottom: 8),
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(75, 155, 95, 18),
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(12),
+                topLeft: Radius.circular(12),
+                bottomLeft: Radius.circular(12)),
+          ),
+          child: Text(
+            "$chattext",
+            style: GoogleFonts.notoSansKannada(
+              fontSize: 15,
+              color: const Color.fromARGB(172, 0, 0, 0),
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ),
+    ],
+  );
 }
