9e0c712d0050a5a6e197f8dd013da4996fcd28b6
hyunwoolee99 <gusdn05229@gmail.com>
2023-04-17 16:09:17 +0900

    Design : juniorcurrent UI design

--- ajousenior/lib/screen/juniorcurrent_screen.dart
+++ ajousenior/lib/screen/juniorcurrent_screen.dart
@@ -18,6 +18,7 @@ class CurrentScreen extends StatelessWidget {
           ),
         ),
       ),
+      backgroundColor: const Color.fromARGB(255, 237, 255, 240),
       body: CurrentList(),
     );
   }
@@ -28,6 +29,7 @@ class CurrentList extends StatelessWidget {
     const Color.fromARGB(147, 94, 222, 102),
     const Color.fromARGB(200, 76, 181, 222),
     const Color.fromARGB(176, 218, 113, 113),
+    const Color.fromARGB(255, 200, 255, 0),
     const Color.fromARGB(200, 174, 124, 207),
   ];
   CurrentList({
@@ -36,55 +38,78 @@ class CurrentList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      padding: const EdgeInsets.all(8),
+    return ListView.builder(
+      /*padding: const EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 3,
+      ),*/
       itemCount: testData.volunteerList.length,
       itemBuilder: (BuildContext context, int index) {
         return ListTile(
           title: Container(
-            padding: const EdgeInsets.symmetric(vertical: 5),
+            padding: const EdgeInsets.symmetric(vertical: 2),
             height: 130,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  testData
-                      .volunteerList[testData.volunteerList.length - index - 1]
-                      .place,
-                  style: const TextStyle(
-                    fontSize: 20,
+            width: double.maxFinite,
+            child: Card(
+              elevation: 5,
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    left: BorderSide(
+                        width: 9.0, color: colorList[index % colorList.length]),
                   ),
-                  overflow: TextOverflow.ellipsis,
+                  color: Colors.white,
+                  //borderRadius: BorderRadius.circular(1),
                 ),
-                const SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  testData
-                      .volunteerList[testData.volunteerList.length - index - 1]
-                      .userID,
-                ),
-                Text(
-                  testData
-                      .volunteerList[testData.volunteerList.length - index - 1]
-                      .date,
-                ),
-                Text(
-                  testData
-                      .volunteerList[testData.volunteerList.length - index - 1]
-                      .content,
-                  style: TextStyle(
-                    color: Colors.black.withOpacity(0.5),
+                child: Padding(
+                  padding: const EdgeInsets.all(7),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        testData
+                            .volunteerList[
+                                testData.volunteerList.length - index - 1]
+                            .place,
+                        style: const TextStyle(
+                          fontSize: 20,
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        testData
+                            .volunteerList[
+                                testData.volunteerList.length - index - 1]
+                            .userID,
+                      ),
+                      Text(
+                        testData
+                            .volunteerList[
+                                testData.volunteerList.length - index - 1]
+                            .date,
+                      ),
+                      Text(
+                        testData
+                            .volunteerList[
+                                testData.volunteerList.length - index - 1]
+                            .content,
+                        style: TextStyle(
+                          color: Colors.black.withOpacity(0.5),
+                        ),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
                   ),
-                  overflow: TextOverflow.ellipsis,
                 ),
-              ],
+              ),
             ),
           ),
         );
       },
-      separatorBuilder: (BuildContext context, int index) => const Divider(),
     );
   }
 }
