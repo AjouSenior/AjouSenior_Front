c5dcb07dc7ebf41ba9738164989a8f66050e2298
KoKimss <seungsu8848@ajou.ac.kr>
2023-05-22 20:11:10 +0900

    fix : myprofile sync 문제 해결

--- ajousenior/lib/models/senior_model.dart
+++ ajousenior/lib/models/senior_model.dart
@@ -45,3 +45,16 @@ Senior StringTo(String a) {
   return Senior(result[0], result[1], result[2], result[3], result[4],
       result[5], result[6]);
 }
+
+Future<Senior> StringTo2(String a) async {
+  List<String> result = a.split('//');
+  return Senior(
+    result[0],
+    result[1],
+    result[2],
+    result[3],
+    result[4],
+    result[5],
+    result[6],
+  );
+}
--- ajousenior/lib/screen/myprofile_screen.dart
+++ ajousenior/lib/screen/myprofile_screen.dart
@@ -1,7 +1,7 @@
+import 'package:ajousenior/models/senior_model.dart';
+import 'package:ajousenior/widgets/logout_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:ajousenior/models/senior_model.dart';
-import '../widgets/logout_widget.dart';
 
 class MyProfileScreen extends StatefulWidget {
   const MyProfileScreen({super.key});
@@ -13,29 +13,111 @@ class MyProfileScreen extends StatefulWidget {
 class _MyProfileScreenState extends State<MyProfileScreen> {
   static var storage = const FlutterSecureStorage();
   dynamic userInfo = '';
-  late Senior user;
+
+  Future<void> _asyncMethod() async {
+    // read 함수로 key값에 맞는 정보를 불러오고 데이터타입은 String 타입
+    userInfo = await storage.read(key: 'login');
+    setState(() {}); // userInfo가 업데이트 되었으므로 setState() 호출
+  }
 
   @override
   void initState() {
     super.initState();
-    // 비동기로 flutter secure storage 정보를 불러오는 작업
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      //_asyncMethod();
-      /*if (userInfo != '') {
-        user = StringTo(userInfo);
-      }*/
+      _asyncMethod();
     });
-  }
-
-  Future<Senior> _asyncMethod() async {
-    // read 함수로 key값에 맞는 정보를 불러오고 데이터타입은 String 타입
-    userInfo = await storage.read(key: 'login');
-    user = StringTo(userInfo);
-    return user;
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget body;
+
+    if (userInfo == null || userInfo.isEmpty) {
+      body = const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else {
+      Senior a = StringTo(userInfo);
+      // userInfo가 값이 있는 경우 body를 구성하는 코드
+      body = Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: [
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              height: 130,
+              width: double.maxFinite,
+              child: Card(
+                elevation: 5,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      left: BorderSide(
+                        width: 9.0,
+                        color: Colors.black,
+                      ),
+                    ),
+                    color: Colors.white,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(7),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              width: 95,
+                              height: 95,
+                              decoration: BoxDecoration(
+                                color: Colors.blue,
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                              child: const Icon(
+                                Icons.person,
+                                color: Colors.white,
+                                size: 80,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 16,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  a.profile_nickname!,
+                                  style: const TextStyle(
+                                    fontSize: 30,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                const SizedBox(
+                                  height: 15,
+                                ),
+                                Text(
+                                  a.account_email!,
+                                  style: const TextStyle(
+                                    fontSize: 25,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const LogOut()
+          ]));
+    }
+
     return WillPopScope(
       onWillPop: () async {
         return false; // 뒤로가기 버튼을 무시하고 아무 동작도 하지 않음
@@ -53,159 +135,7 @@ class _MyProfileScreenState extends State<MyProfileScreen> {
             ),
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 5),
-                height: 130,
-                width: double.maxFinite,
-                child: Card(
-                  elevation: 5,
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        left: BorderSide(
-                          width: 9.0,
-                          color: Colors.black,
-                        ),
-                      ),
-                      color: Colors.white,
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(7),
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Container(
-                                width: 95,
-                                height: 95,
-                                decoration: BoxDecoration(
-                                  color: Colors.blue,
-                                  borderRadius: BorderRadius.circular(30),
-                                ),
-                                child: const Icon(
-                                  Icons.person,
-                                  color: Colors.white,
-                                  size: 80,
-                                ),
-                              ),
-                              const SizedBox(
-                                width: 16,
-                              ),
-<<<<<<< HEAD
-                              const Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Name',
-                                    style: TextStyle(
-                                      fontSize: 30,
-                                    ),
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                  SizedBox(
-                                    height: 15,
-                                  ),
-                                  Text(
-                                    'test',
-                                    style: TextStyle(
-                                      fontSize: 25,
-                                    ),
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ],
-=======
-                              FutureBuilder<Senior>(
-                                future: _asyncMethod(),
-                                builder: (context, snapshot) {
-                                  if (snapshot.connectionState ==
-                                      ConnectionState.waiting) {
-                                    return const CircularProgressIndicator();
-                                  } else if (snapshot.hasError) {
-                                    return Text('${snapshot.error}');
-                                  } else if (!snapshot.hasData) {
-                                    return const Text('No data');
-                                  } else {
-                                    //final user = snapshot.data!;
-                                    return Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          user.profile_nickname!,
-                                          style: const TextStyle(
-                                            fontSize: 30,
-                                          ),
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                        const SizedBox(
-                                          height: 20,
-                                        ),
-                                        Text(
-                                          user.account_email!,
-                                          style: const TextStyle(
-                                            fontSize: 25,
-                                          ),
-                                          overflow: TextOverflow.ellipsis,
-                                        ),
-                                      ],
-                                    );
-                                  }
-                                },
->>>>>>> 28ea7766ae225b5f4102d3986e881ade3bb0a47d
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Expanded(
-                child: ListView.separated(
-                  itemCount: 3,
-                  itemBuilder: (context, index) => ListTile(
-                    title: Container(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      height: 120,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            'text',
-                            style: TextStyle(
-                              fontSize: 30,
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 30,
-                          ),
-                          Text(
-                            'text',
-                            style: TextStyle(
-                              color: Colors.black.withOpacity(0.5),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  separatorBuilder: (context, index) => const Divider(),
-                ),
-              ),
-              const LogOut(),
-            ],
-          ),
-        ),
+        body: body,
       ),
     );
   }
